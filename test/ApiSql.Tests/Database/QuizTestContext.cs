8b40397e98eb888a0c409ff2efaf9467cc20d4a3
brunodavi <48166556+brunodavi@users.noreply.github.com>
2025-12-01 00:24:01 -0300

    feat: adicionar contexto de teste para quizzes com suporte a banco de dados em memória

---
+++ test/ApiSql.Tests/Database/QuizTestContext.cs
@@ -0,0 +1,21 @@
+using ApiSql.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSql.Tests.Database;
+
+public class QuizTestContext : QuizContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // var serviceProvider = new ServiceCollection()
+        //     .AddEntityFrameworkInMemoryDatabase()
+        //     .BuildServiceProvider();
+
+        // optionsBuilder
+        //     .UseInMemoryDatabase("Quizzes")
+        //     .UseInternalServiceProvider(serviceProvider);
+
+        optionsBuilder.UseInMemoryDatabase("Quizzes")
+            .UseLazyLoadingProxies();
+    }
+}
